f8f818a905fb7846984350c126c64e504cd49e61
zulpikaruygur <zulfikaruygur@gmail.com>
2021-02-11 14:25:34 +0300

    test 4 added

---
+++ src/test/java/com/vytrack/tests/Test4.java
@@ -0,0 +1,26 @@
+package com.vytrack.tests;
+
+import com.vytrack.pages.LoginPage;
+import com.vytrack.utilities.ConfigurationReader;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class Test4 extends TestBase {
+
+
+    @Test
+    public void test4() throws InterruptedException{
+        LoginPage loginPage = new LoginPage();
+        loginPage.login(ConfigurationReader.get("username"),ConfigurationReader.get("password"));
+        Actions action = new Actions(driver);
+        WebElement activities = driver.findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[5]/a/span"));
+        action.moveToElement(activities).perform();
+        WebElement events = driver.findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[5]/div/div/ul/li[4]/a/span"));
+        Thread.sleep(5000);
+        action.moveToElement(events).click().perform();
+        Thread.sleep(10000);
+    }
+
+}
